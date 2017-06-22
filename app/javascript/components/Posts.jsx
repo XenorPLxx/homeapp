2e68d89dee38a25e0c4e6b27cd8bdcc863997ec1
XenorPLxx <dawidxen@gmail.com>
2017-06-23 01:22:26 +0200

    React+Webpacker figured out.

---
+++ app/javascript/components/Posts.jsx
@@ -0,0 +1,11 @@
+var React = require("react")
+class Posts extends React.Component {
+  render() {
+   return (<div id="one-time-click-link">
+     <a href="javascript:void(0)">
+       Click me
+     </a>
+   </div>);
+  }
+}
+export default Posts;
---
+++ app/javascript/components/Title.jsx
@@ -0,0 +1,7 @@
+var React = require("react")
+class Title extends React.Component {
+  render() {
+    return (<h1>AAAA LOOK IM REACT COMPONENT</h1>);
+  }
+}
+export default Title;
